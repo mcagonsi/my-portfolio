3375b8e1d54c23baf71f36b2228218efb4cc6fca
Michael Agonsi <144360839+mcagonsi@users.noreply.github.com>
2025-04-29 13:24:26 -0230

    will continue with database connection from here

--- src/components/ProjectsServer.tsx
+++ src/components/ProjectsServer.tsx
@@ -1,10 +1,10 @@
 // app/components/ProjectsServer.tsx
 
-import connectToDB from "@/lib/data";
+// import connectToDB from "@/lib/data";
 import Projects from "@/components/Projects";
 
 export default async function ProjectsServer() {
-    const client = connectToDB() // your database fetch here
-
-    return <Projects projects={client} />;
+    // const client = connectToDB() // your database fetch here
+    return <Projects />;
+    // return <Projects projects={client} />;
 }
