57e10c7ef15af493ba7f945d2da0a2477a16e105
Michael Agonsi <144360839+mcagonsi@users.noreply.github.com>
2025-05-04 11:05:39 -0230

    testing vercel db connection

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,7 @@
 // lib/db.ts
 import { Pool } from 'pg';
+console.log("POSTGRES_URL on Vercel:", process.env.POSTGRES_URL);
+
 
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
