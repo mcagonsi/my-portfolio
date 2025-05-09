48b8ebffffe5c17ad1f6221d1f08e63b252f1d02
Michael Agonsi <144360839+mcagonsi@users.noreply.github.com>
2025-05-09 00:16:02 -0230

    worked on redesigning the ux planning to implement dark and light themes later

--- src/app/projects/components/ProjectCard.tsx
+++ src/app/projects/components/ProjectCard.tsx
@@ -14,26 +14,26 @@ export default function ProjectCard({ project }: { project: project }) {
 
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
-            transition={{ duration: 0.5, delay: 1 }} key={project.id} className="flex flex-col items-center justify-center sm:w-full bg-white rounded-md  hover:shadow-white hover:shadow-md hover:scale-105 transition-transform duration-300 ease-in-out cursor-pointer p-2 mb-4">
-            <div onClick={handleClick} className="flex flex-col justify-center w-full h-full hover:shadow-white hover:shadow-md hover:scale">
+            transition={{ duration: 0.5, delay: 1 }} key={project.id} className="flex flex-col items-center justify-center sm:w-full bg-black rounded-md  hover:shadow-white hover:shadow-md hover:scale-105 transition-transform duration-300 ease-in-out cursor-pointer p-2 mb-4">
+            <div onClick={handleClick} className="flex flex-col justify-center w-full h-full">
                 <Image src={project.image} alt={project.title} width={300} height={300} className="  w-full h-full object-cover rounded-lg mb-2" />
-                <h3 className="text-lg text-start font-semibold text-gray-400">Project: <span className="text-black">{project.title}</span> </h3>
-                <p className="text-gray-400">Description: <span className="text-black">{project.description}</span> </p>
-                <p className="text-gray-400">Tech Stack Used: <span className="text-black">{project.techStack}</span></p>
-                <p className="text-gray-400">Date Posted: <span className="text-black">{new Date(project.timeposted).toISOString().split('T')[0]}</span></p>
-                <p className="text-gray-400">Summary: <span className="text-black">{project.summary}</span></p>
+                <h3 className="text-lg text-start font-semibold text-blue-400">Project: <span className="text-white">{project.title}</span> </h3>
+                <p className="text-blue-400">Description: <span className="text-gray-500">{project.description}</span> </p>
+                <p className="text-blue-400">Tech Stack Used: <span className="text-gray-500 flex flex-wrap gap-2">{project.techStack.split(' ').map((skill, index) => (<span key={index} className="p-1 bg-black text-gray-400 border-gray-400 border rounded-sm hover:bg-white transition duration-300 ease-in-out hover:text-black">{skill}</span>))}</span></p>
+                <p className="text-blue-400">Date Posted: <span className="text-gray-500">{new Date(project.timeposted).toISOString().split('T')[0]}</span></p>
+                <p className="text-blue-400">Summary: <span className="text-gray-500">{project.summary}</span></p>
                 <div className="flex flex-row items-start justify-start mt-2 space-x-2">
                     <Link href={project.linkToRepository} className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">💻View Project</Link>
                     <Link href={project.linkToRepository} className="px-4 py-2 bg-gray-700 text-white rounded-md hover:bg-gray-800">🔗View Code</Link>
 
                 </div>
                 <div className="flex flex-row justify-between items-center">
-                    <span className='text-white text-upperCase w-fit item-end px-4 py-2 mt-10 rounded-sm bg-black'>{project.category}</span>
+                    <span className='text-black text-upperCase w-fit item-end px-4 py-2 mt-10 rounded-sm bg-white'>{project.category}</span>
                     {project.isFeatured && <span className='text-white text-4xl w-fit item-end px-4 py-2 mt-10 rounded-sm'>🔥</span>}
                 </div>
 
-            </div>
+            </div >
 
 
-        </motion.div>)
+        </motion.div >)
 }   
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -7,7 +7,7 @@ import ProjectCatalog from "./components/ProjectCatalog";
 export default async function ProjectsPage() {
     return (
         <main className="flex flex-col items-center justify-center min-h-screen bg-black text-white p-4">
-            <h2 className="text-2xl font-semibold mb-4">🔥 Projects Catalog 💼</h2>
+            <h2 className="text-2xl font-semibold mb-16">🔥 Projects Catalog 💼</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full max-w-screen-xl px-4">
                 <ProjectCatalog />  {/* Project cards will go here */}
             </div>
